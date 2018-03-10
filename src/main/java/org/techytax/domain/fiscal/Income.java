19fd7d8ed7ac1fdbc6f5555c6383f5d7fd33bdda
Hans Beemsterboer <hpbeem@gmail.com>
2018-03-10 18:36:02 +0100

    [IB 2017]: return invoices

--- src/main/java/org/techytax/domain/fiscal/Income.java
+++ src/main/java/org/techytax/domain/fiscal/Income.java
@@ -1,28 +1,31 @@
 package org.techytax.domain.fiscal;
 
+import lombok.Data;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.techytax.domain.Cost;
 import org.techytax.domain.Invoice;
 import org.techytax.repository.InvoiceRepository;
 
 import java.math.BigDecimal;
 import java.math.BigInteger;
-import java.util.Iterator;
+import java.time.LocalDate;
 import java.util.List;
 
 import static java.math.BigInteger.ZERO;
 
+@Data
 @Component
 public class Income {
 
-  public BigInteger nettoOmzet = ZERO;
+  private BigInteger nettoOmzet = ZERO;
+
+  private List<Invoice> invoices;
 
   @Autowired
   private InvoiceRepository invoiceRepository;
 
   public BigDecimal calculateNetIncome(String username) {
-    Iterable<Invoice> invoices = invoiceRepository.findByUser(username);
+    invoices = (List<Invoice>) invoiceRepository.findInvoices(username, LocalDate.now().minusYears(1).withMonth(1).withDayOfMonth(1), LocalDate.now().withMonth(1).withDayOfMonth(1).minusDays(1));
     BigDecimal totalNetIncome = BigDecimal.ZERO;
     for (Invoice invoice : invoices) {
       totalNetIncome = totalNetIncome.add(BigDecimal.valueOf(invoice.getUnitsOfWork()).multiply(invoice.getProject().getRate()));
