dca5ef4686daaa9eea9b39ebb8c06f0188c99734
Hans Beemsterboer <Hans.Beemsterboer@rabobank.nl>
2020-04-13 14:01:15 +0200

    Added credit invoice

--- src/main/java/org/techytax/domain/Invoice.java
+++ src/main/java/org/techytax/domain/Invoice.java
@@ -7,6 +7,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import javax.persistence.Transient;
 import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 
@@ -31,7 +32,10 @@ public class Invoice {
     private String month;
 
     private float unitsOfWork;
+
+    @Transient
     private String originalInvoiceNumber;
+
     private int discountPercentage;
 
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy/MM/dd")
